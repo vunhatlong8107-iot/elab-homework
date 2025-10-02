dddd607b33febe104a1567917ad70b2f83c2a778
KimSondabe <vukimsonmint@gmail.com>
2025-10-02 23:39:08 +0700

    Bai Mode(not done yet)

--- homewwork.c
+++ homewwork.c
@@ -1,32 +1,41 @@
 #include<stdio.h>
 #include<math.h>
 
+//DISPLAY MODE
+
 int main(){
-    float battery, temp, current;
-    printf("Nhap nhiet do pin "); scanf("%f", &temp); fflush(stdin);
-    printf("------------------------------\n");
-    printf("Nhap phan tram pin "); scanf("%f", &battery); fflush(stdin);
-    printf("------------------------------\n");
-    printf("Nhap dien ap "); scanf("%f", &current); fflush(stdin);
-    printf("------------THONG-BAO----------\n");
-    if(temp < 60){
-        if(battery >= 100){
-            printf("Ngat sac - Day pin !!!");
-        }else if(battery >= 80){
-            if(current > 20){
-                printf("Giam dong sac(trickle charge)");
-            }else {
-                printf("Sac binh thuong");
-            }
-        }else if(battery <= 10 ){
-            printf("Sac nhanh (quick charge)");
-        }
-        else if(battery > 10){
-            printf("Sac binh thuong");
-        }
-    }
-    else{
-        printf("Dung sac - Qua nhiet");
+    int mode, battery;
+    printf("**************************\n");
+    printf("0. Standby\n");
+    printf("1. Text\n");
+    printf("2. Video\n");
+    printf("3. Gaming\n");
+    printf("Nhap mode ban muon chon\n");
+    printf("4.Power Saving\n");
+    printf("5. High Performance\n");
+    printf("**************************\n");
+    scanf("%d", mode);
+    switch (mode){
+    case 0:
+        printf("Standby");
+        break;
+    case 1:
+        printf("Text");
+        break;
+    case 2:
+        printf("Video");
+        break;
+    case 3:
+        printf("Game");
+        break;
+    case 4:
+        printf("Power Saving");
+        break;
+    case 5:
+        printf("High Performance");
+        break;
+    default:
+        break;
     }
     return 0;
 }
