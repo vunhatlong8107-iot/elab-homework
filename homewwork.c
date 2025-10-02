0734590867cba04acd22c52fe528f24f0da22725
KimSondabe <vukimsonmint@gmail.com>
2025-10-02 23:03:34 +0700

    Bai Pin Xe

--- homewwork.c
+++ homewwork.c
@@ -1,32 +1,32 @@
 #include<stdio.h>
 #include<math.h>
-#define PI 3.142
-int main(){
-    float km_h;
-    printf("Nhap toc do cua ban (km/h) ");
-    scanf("%f", &km_h);
-    fflush(stdin);
-    float m_s = km_h / 3.6;
-    printf("Toc do ban theo km/h la %.3f km/h\n", km_h);
-    printf("Toc do ban theo m/s la %.3f m/s\n", m_s);
-    fflush(stdout);
-    printf("************************************************************************\n");
-    
-    float r;
-    printf("Nhap ban kinh hinh tron ");
-    scanf("%f", &r);
-    fflush(stdin);
-    float C = 2 * PI * r;
-    float S = PI * r * r;
-    printf("Hinh tron voi ban kinh %.3f co chu vi la %.3f va dien tich %.3f\n", r, C, S);
-    fflush(stdout);
 
-    printf("************************************************************************\n");
-   
-    float dai = 4;
-    float rong = 30/dai;
-    float rong2 = floor(rong);
-    printf("Chieu rong la %.2f va lam tron la %.0f", rong, rong2);
-    
+int main(){
+    float battery, temp, current;
+    printf("Nhap nhiet do pin "); scanf("%f", &temp); fflush(stdin);
+    printf("------------------------------\n");
+    printf("Nhap phan tram pin "); scanf("%f", &battery); fflush(stdin);
+    printf("------------------------------\n");
+    printf("Nhap dien ap "); scanf("%f", &current); fflush(stdin);
+    printf("------------THONG-BAO----------\n");
+    if(temp < 60){
+        if(battery >= 100){
+            printf("Ngat sac - Day pin !!!");
+        }else if(battery >= 80){
+            if(current > 20){
+                printf("Giam dong sac(trickle charge)");
+            }else {
+                printf("Sac binh thuong");
+            }
+        }else if(battery <= 10 ){
+            printf("Sac nhanh (quick charge)");
+        }
+        else if(battery > 10){
+            printf("Sac binh thuong");
+        }
+    }
+    else{
+        printf("Dung sac - Qua nhiet");
+    }
     return 0;
 }
